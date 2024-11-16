4ab0ab276bedd33b1106379f474d48e2db9e5976
Giovanny Prens Luna <69285209+GPrensLuna@users.noreply.github.com>
2024-11-16 18:46:40 -0500

    Update eslint.sh

--- sh/eslint.sh
+++ sh/eslint.sh
@@ -29,59 +29,67 @@ ask_project_type() {
 # ------------------------------------------------------------------------------
 #* 3. Función para generar la configuración de eslint para Next.js
 generate_next_eslint() {
-    echo 'import globals from 'globals'
-import tseslint from 'typescript-eslint'
-import eslintPluginReact from 'eslint-plugin-react'
-import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
-import { fixupPluginRules } from '@eslint/compat'
-import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
-import eslintPluginReactCompiler from 'eslint-plugin-react-compiler'
-import eslintPluginImport from 'eslint-plugin-import'
-import eslintPluginUnusedImport from 'eslint-plugin-unused-imports'
-import eslintPluginNext from '@next/eslint-plugin-next'
-import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y'
-import vercelStyleGuideTypescript from '@vercel/style-guide/eslint/typescript'
-import vercelStyleGuideReact from '@vercel/style-guide/eslint/rules/react'
-import vercelStyleGuideNext from '@vercel/style-guide/eslint/next'
+    echo 'import { fixupPluginRules } from "@eslint/compat";
+import eslintPluginNext from "@next/eslint-plugin-next";
+import vercelStyleGuideNext from "@vercel/style-guide/eslint/next";
+import vercelStyleGuideReact from "@vercel/style-guide/eslint/rules/react";
+import vercelStyleGuideTypescript from "@vercel/style-guide/eslint/typescript";
+import eslintPluginImport from "eslint-plugin-import";
+import eslintPluginJsxA11y from "eslint-plugin-jsx-a11y";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginReactCompiler from "eslint-plugin-react-compiler";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+import eslintPluginUnusedImport from "eslint-plugin-unused-imports";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default [
 	// Ignores configuration
 	{
-		ignores: ['node_modules', '.next', 'out', 'coverage', '.idea'],
+		ignores: [
+			"node_modules",
+			".next",
+			"out",
+			"coverage",
+			".idea",
+			"src/UI/components/ui/**.tsx",
+		],
 	},
 	// General configuration
 	{
 		rules: {
-			'padding-line-between-statements': [
-				'warn',
-				{ blankLine: 'always', prev: '*', next: ['return', 'export'] },
+			"padding-line-between-statements": [
+				"warn",
+				{ blankLine: "always", prev: "*", next: ["return", "export"] },
 				{
-					blankLine: 'always',
-					prev: ['const', 'let', 'var'],
-					next: '*',
+					blankLine: "always",
+					prev: ["const", "let", "var"],
+					next: "*",
 				},
 				{
-					blankLine: 'any',
-					prev: ['const', 'let', 'var'],
-					next: ['const', 'let', 'var'],
+					blankLine: "any",
+					prev: ["const", "let", "var"],
+					next: ["const", "let", "var"],
 				},
 			],
-			'no-console': 'warn',
-			'no-alert': 'warn', // Evita el uso de `alert`, `confirm`, etc.
-			'no-debugger': 'warn', // Evita el uso de `debugger`
-			'no-empty-function': 'warn', // Previene funciones vacías
-			'prefer-const': 'warn', // Asegura el uso de `const` cuando sea posible
-			'consistent-return': 'warn', // Asegura que las funciones tengan un `return` consistente
-			eqeqeq: 'warn', // Asegura el uso de `===` y `!==` en lugar de `==` y `!=`
+			"no-console": "warn",
+			"no-alert": "warn", // Evita el uso de `alert`, `confirm`, etc.
+			"no-debugger": "warn", // Evita el uso de `debugger`
+			"no-empty-function": "warn", // Previene funciones vacías
+			"prefer-const": "warn", // Asegura el uso de `const` cuando sea posible
+			"consistent-return": "warn", // Asegura que las funciones tengan un `return` consistente
+			eqeqeq: "warn", // Asegura el uso de `===` y `!==` en lugar de `==` y `!=`
+			"lines-between-class-members": "warn", // Evita espacios en blanco entre clases
 		},
 	},
 	// React configuration
 	{
 		plugins: {
 			react: fixupPluginRules(eslintPluginReact),
-			'react-hooks': fixupPluginRules(eslintPluginReactHooks),
-			'react-compiler': fixupPluginRules(eslintPluginReactCompiler),
-			'jsx-a11y': fixupPluginRules(eslintPluginJsxA11y),
+			"react-hooks": fixupPluginRules(eslintPluginReactHooks),
+			"react-compiler": fixupPluginRules(eslintPluginReactCompiler),
+			"jsx-a11y": fixupPluginRules(eslintPluginJsxA11y),
 		},
 		languageOptions: {
 			parserOptions: {
@@ -96,7 +104,7 @@ export default [
 		},
 		settings: {
 			react: {
-				version: 'detect',
+				version: "detect",
 			},
 		},
 		rules: {
@@ -104,12 +112,12 @@ export default [
 			...eslintPluginJsxA11y.configs.recommended.rules,
 			...eslintPluginReactHooks.configs.recommended.rules,
 			...vercelStyleGuideReact.rules,
-			'react/prop-types': 'off',
-			'react/jsx-uses-react': 'off',
-			'react/react-in-jsx-scope': 'off',
-			'react/self-closing-comp': 'warn',
-			'react/jsx-sort-props': [
-				'warn',
+			"react/prop-types": "off",
+			"react/jsx-uses-react": "off",
+			"react/react-in-jsx-scope": "off",
+			"react/self-closing-comp": "warn",
+			"react/jsx-sort-props": [
+				"warn",
 				{
 					callbacksLast: true,
 					shorthandFirst: true,
@@ -117,24 +125,24 @@ export default [
 					reservedFirst: true,
 				},
 			],
-			'react/function-component-definition': [
-				'warn',
+			"react/function-component-definition": [
+				"warn",
 				{
-					namedComponents: 'arrow-function',
-					unnamedComponents: 'arrow-function',
+					namedComponents: "arrow-function",
+					unnamedComponents: "arrow-function",
 				},
 			],
-			'react-compiler/react-compiler': 'error',
-			'react/jsx-no-leaked-render': 'off',
-			'jsx-a11y/no-static-element-interactions': 'off',
-			'jsx-a11y/click-events-have-key-events': 'off',
-			'jsx-a11y/anchor-is-valid': 'warn', // Asegura que los enlaces sean válidos
-			'jsx-a11y/label-has-associated-control': 'warn', // Asegura que las etiquetas `<label>` tengan controles asociados
-			'jsx-a11y/no-noninteractive-element-interactions': 'warn', // Previene el uso de elementos no interactivos con interacciones
-			'jsx-a11y/no-redundant-roles': 'warn', // Evita roles redundantes en los elementos
-			'jsx-a11y/accessible-emoji': 'warn', // Asegura que los emojis sean accesibles
-			'jsx-a11y/no-static-element-interactions': 'warn', // Evita el uso de elementos estáticos con interacciones
-			'jsx-a11y/click-events-have-key-events': 'warn', // Asegura que los eventos de clic también tengan eventos de teclado
+			"react-compiler/react-compiler": "error",
+			"react/jsx-no-leaked-render": "off",
+			"jsx-a11y/no-static-element-interactions": "off",
+			"jsx-a11y/click-events-have-key-events": "off",
+			"jsx-a11y/anchor-is-valid": "warn", // Asegura que los enlaces sean válidos
+			"jsx-a11y/label-has-associated-control": "warn", // Asegura que las etiquetas `<label>` tengan controles asociados
+			"jsx-a11y/no-noninteractive-element-interactions": "warn", // Previene el uso de elementos no interactivos con interacciones
+			"jsx-a11y/no-redundant-roles": "warn", // Evita roles redundantes en los elementos
+			"jsx-a11y/accessible-emoji": "warn", // Asegura que los emojis sean accesibles
+			"jsx-a11y/no-static-element-interactions": "warn", // Evita el uso de elementos estáticos con interacciones
+			"jsx-a11y/click-events-have-key-events": "warn", // Asegura que los eventos de clic también tengan eventos de teclado
 		},
 	},
 	// TypeScript configuration
@@ -143,43 +151,41 @@ export default [
 		{
 			rules: {
 				...vercelStyleGuideTypescript.rules,
-				'@typescript-eslint/no-non-null-assertion': 'off', // Evita el uso de `!` en declaraciones de variables
-				'@typescript-eslint/no-shadow': 'off', // Evita el uso de `var` en declaraciones de variables
-				'@typescript-eslint/explicit-function-return-type': 'off', // Evita el uso de `any` en declaraciones de funciones
-				'@typescript-eslint/require-await': 'off', // Evita el uso de promesas sin await en funciones
-				'@typescript-eslint/no-floating-promises': 'off', // Evita el uso de promesas sin await en funciones
-				'@typescript-eslint/no-confusing-void-expression': 'off', // Evita el uso de `void` en declaraciones de variables
-				'@typescript-eslint/explicit-module-boundary-types': 'error', // Requerir declaraciones de tipos de retorno en las funciones
-				'@typescript-eslint/no-explicit-any': 'error', // Requerir declaraciones de tipos de retorno en las funciones
-				'@typescript-eslint/no-inferrable-types': 'error', // Requerir declaraciones de tipos de retorno en las funciones
-				'@typescript-eslint/explicit-function-return-type': 'error', // Requerir declaraciones de tipos de retorno en las funciones
-				'@typescript-eslint/no-empty-interface': 'error', // Requerir declaraciones de tipos de retorno en las funciones
-				'@typescript-eslint/ban-ts-comment': 'error', // Evita los comentarios de `@ts-ignore` sin justificación
+				"@typescript-eslint/no-non-null-assertion": "off", // Evita el uso de `!` en declaraciones de variables
+				"@typescript-eslint/no-shadow": "off", // Evita el uso de `var` en declaraciones de variables
+				"@typescript-eslint/explicit-function-return-type": "off", // Evita el uso de `any` en declaraciones de funciones
+				"@typescript-eslint/require-await": "off", // Evita el uso de promesas sin await en funciones
+				"@typescript-eslint/no-floating-promises": "off", // Evita el uso de promesas sin await en funciones
+				"@typescript-eslint/no-confusing-void-expression": "off", // Evita el uso de `void` en declaraciones de variables
+				"@typescript-eslint/explicit-module-boundary-types": "error", // Requerir declaraciones de tipos de retorno en las funciones
+				"@typescript-eslint/no-explicit-any": "error", // Requerir declaraciones de tipos de retorno en las funciones
+				"@typescript-eslint/no-inferrable-types": "error", // Requerir declaraciones de tipos de retorno en las funciones
+				"@typescript-eslint/explicit-function-return-type": "error", // Requerir declaraciones de tipos de retorno en las funciones
+				"@typescript-eslint/no-empty-interface": "error", // Requerir declaraciones de tipos de retorno en las funciones
+				"@typescript-eslint/ban-ts-comment": "error", // Evita los comentarios de `@ts-ignore` sin justificación
 
-				'@typescript-eslint/consistent-type-definitions': [
-					'error',
-					'interface',
-				], // Requerir declaraciones de tipos de retorno en las funciones
-				'@typescript-eslint/member-ordering': 'error', // Requerir declaraciones de tipos de retorno en las funciones
-				'@typescript-eslint/no-magic-numbers': ['error', { ignore: [0, 1] }], // Evita números mágicos
-				'@typescript-eslint/no-this-alias': 'error', // Evita el uso de `this` en declaraciones de variables
-				'@typescript-eslint/adjacent-overload-signatures': 'error', // Evita el uso de `=` en declaraciones de variables
-				'@typescript-eslint/class-literal-property-style': 'error',
-				'@typescript-eslint/consistent-generic-constructors': 'error',
-				'@typescript-eslint/consistent-indexed-object-style': 'error',
-				'@typescript-eslint/consistent-type-assertions': 'error',
-				'@typescript-eslint/consistent-type-imports': 'error',
-				'@typescript-eslint/explicit-member-accessibility': 'error',
-				'@typescript-eslint/method-signature-style': 'error',
-				'@typescript-eslint/no-confusing-non-null-assertion': 'error',
-				'@typescript-eslint/no-duplicate-enum-values': 'error',
-				'@typescript-eslint/no-extra-non-null-assertion': 'error',
-				'@typescript-eslint/no-for-in-array': 'error',
-				'@typescript-eslint/no-import-type-side-effects': 'error',
-				'@typescript-eslint/no-require-imports': 'error',
-				'@typescript-eslint/no-unnecessary-type-assertion': 'off',
-				'@typescript-eslint/no-non-null-assertion': 'error',
-				'@typescript-eslint/unified-signatures': 'off',
+				"@typescript-eslint/consistent-type-definitions": ["error", "interface"], // Requerir declaraciones de tipos de retorno en las funciones
+				"@typescript-eslint/member-ordering": "error", // Requerir declaraciones de tipos de retorno en las funciones
+				"@typescript-eslint/no-magic-numbers": ["error", { ignore: [0, 1] }], // Evita números mágicos
+				"@typescript-eslint/no-this-alias": "error", // Evita el uso de `this` en declaraciones de variables
+				"@typescript-eslint/adjacent-overload-signatures": "error", // Evita el uso de `=` en declaraciones de variables
+				"@typescript-eslint/class-literal-property-style": "error",
+				"@typescript-eslint/consistent-generic-constructors": "error",
+				"@typescript-eslint/consistent-indexed-object-style": "error",
+				"@typescript-eslint/consistent-type-assertions": "error",
+				"@typescript-eslint/consistent-type-imports": "error",
+				"@typescript-eslint/explicit-member-accessibility": "error",
+				"@typescript-eslint/method-signature-style": "error",
+				"@typescript-eslint/no-confusing-non-null-assertion": "error",
+				"@typescript-eslint/no-duplicate-enum-values": "error",
+				"@typescript-eslint/no-extra-non-null-assertion": "error",
+				"@typescript-eslint/no-for-in-array": "error",
+				"@typescript-eslint/no-import-type-side-effects": "error",
+				"@typescript-eslint/no-require-imports": "error",
+				"@typescript-eslint/no-unnecessary-type-assertion": "off",
+				"@typescript-eslint/no-non-null-assertion": "error",
+				"@typescript-eslint/unified-signatures": "off",
+				"@typescript-eslint/no-unused-expressions": "off",
 			},
 		},
 	],
@@ -188,22 +194,22 @@ export default [
 		eslintPluginPrettier,
 		{
 			rules: {
-				'prettier/prettier': [
-					'warn',
+				"prettier/prettier": [
+					"warn",
 					{
-						semi: false,
-						parser: 'typescript',
+						semi: true,
+						parser: "typescript",
 						useTabs: true,
 						tabWidth: 2,
-						proseWrap: 'preserve',
-						endOfLine: 'lf',
-						printWidth: 80,
-						quoteProps: 'as-needed',
+						proseWrap: "preserve",
+						endOfLine: "lf",
+						printWidth: 90,
+						quoteProps: "as-needed",
 						rangeStart: 0,
-						singleQuote: true,
-						arrowParens: 'always',
+						singleQuote: false,
+						arrowParens: "always",
 						insertPragma: false,
-						trailingComma: 'all',
+						trailingComma: "all",
 						requirePragma: false,
 						bracketSpacing: true,
 						jsxSingleQuote: false,
@@ -211,77 +217,77 @@ export default [
 						jsxBracketSameLine: false,
 						singleAttributePerLine: false,
 						vueIndentScriptAndStyle: false,
-						htmlWhitespaceSensitivity: 'css',
-						embeddedLanguageFormatting: 'auto',
-						plugins: ['prettier-plugin-tailwindcss'],
+						htmlWhitespaceSensitivity: "css",
+						embeddedLanguageFormatting: "auto",
+						plugins: ["prettier-plugin-tailwindcss"],
 						overrides: [
 							{
-								files: '*.{ts,tsx}',
+								files: "*.{ts,tsx}",
 								options: {
-									parser: 'typescript',
+									parser: "typescript",
 								},
 							},
 							{
-								files: '*.{js,jsx}',
+								files: "*.{js,jsx}",
 								options: {
-									parser: 'babel',
+									parser: "babel",
 								},
 							},
 							{
-								files: '*.vue',
+								files: "*.vue",
 								options: {
-									parser: 'vue',
+									parser: "vue",
 								},
 							},
 							{
-								files: '*.css',
+								files: "*.css",
 								options: {
-									parser: 'css',
+									parser: "css",
 								},
 							},
 							{
-								files: '*.scss',
+								files: "*.scss",
 								options: {
-									parser: 'scss',
+									parser: "scss",
 								},
 							},
 							{
-								files: '*.less',
+								files: "*.less",
 								options: {
-									parser: 'less',
+									parser: "less",
 								},
 							},
 							{
-								files: '*.json',
+								files: "*.json",
 								options: {
-									parser: 'json',
+									parser: "json",
 								},
 							},
 							{
-								files: '*.md',
+								files: "*.md",
 								options: {
-									parser: 'markdown',
+									parser: "markdown",
 								},
 							},
 							{
-								files: '*.yaml',
+								files: "*.yaml",
 								options: {
-									parser: 'yaml',
+									parser: "yaml",
 								},
 							},
 						],
 						importOrder: [
-							'^react',
-							'^next',
-							'<THIRD_PARTY_MODULES>',
-							'^@/(.*)$',
-							'^[./]',
+							"^react",
+							"^next",
+							"<THIRD_PARTY_MODULES>",
+							"^@/(.*)$",
+							"^[./]",
 						],
 						importOrderSeparation: true,
 						importOrderSortSpecifiers: true,
 						importOrderGroupNamespaceSpecifiers: true,
 						importOrderCaseInsensitive: true,
-						xmlWhitespaceSensitivity: 'strict',
+						xmlWhitespaceSensitivity: "strict",
 						jsonRecursiveSort: true,
 						astroAllowShorthand: false,
 					},
@@ -295,30 +301,30 @@ export default [
 			import: fixupPluginRules(eslintPluginImport),
 		},
 		rules: {
-			'import/no-default-export': 'off', // Evita exportaciones por defecto
-			'import/first': 'warn', // Asegura que las importaciones estén al principio del archivo
-			'import/no-duplicates': 'warn', // Evita importaciones duplicadas
-			'import/order': [
-				'warn',
+			"import/no-default-export": "off", // Evita exportaciones por defecto
+			"import/first": "warn", // Asegura que las importaciones estén al principio del archivo
+			"import/no-duplicates": "warn", // Evita importaciones duplicadas
+			"import/order": [
+				"warn",
 				{
 					groups: [
-						'type',
-						'builtin',
-						'object',
-						'external',
-						'internal',
-						'parent',
-						'sibling',
-						'index',
+						"type",
+						"builtin",
+						"object",
+						"external",
+						"internal",
+						"parent",
+						"sibling",
+						"index",
 					],
 					pathGroups: [
 						{
-							pattern: '~/**',
-							group: 'external',
-							position: 'after',
+							pattern: "~/**",
+							group: "external",
+							position: "after",
 						},
 					],
-					'newlines-between': 'always', // Determina si se usará una nueva línea entre importaciones
+					"newlines-between": "always", // Determina si se usará una nueva línea entre importaciones
 				},
 			],
 		},
@@ -328,8 +334,8 @@ export default [
 			unused: fixupPluginRules(eslintPluginUnusedImport),
 		},
 		rules: {
-			'unused/no-unused-imports': 'warn',
-			'unused/no-unused-vars': 'warn',
+			"unused/no-unused-imports": "warn",
+			"unused/no-unused-vars": "warn",
 		},
 	},
 	// Next configuration
@@ -345,10 +351,11 @@ export default [
 		},
 		rules: {
 			...vercelStyleGuideNext.rules,
-			'next/no-img-element': 'warn', // Evita el uso de `<img />` sin `next/image`
+			"next/no-img-element": "warn", // Evita el uso de `<img />` sin `next/image`
 		},
 	},
-]
+];
+
 
 '
 }
